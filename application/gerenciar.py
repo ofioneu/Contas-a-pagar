2d1358fc617a1b3a24705bf1c359965aa2788a15
Ofioneu <fmfabio.0991@yahoo.com.br>
2020-07-10 18:41:59 -0300

    ajustado layout de forms e feito somatoria de preço no resultado dos filtros

--- application/gerenciar.py
+++ application/gerenciar.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, Blueprint, url_for
 from forms.form import MyForm
 from models.db_contas import db, ContasModel
 from app import app_config
+from sqlalchemy import func
 
 from flask_mail import Mail, Message
 
@@ -12,90 +13,84 @@ app = Flask(__name__, template_folder='templates')
 
 mail = Mail(app)
 
+
 @gerenciar.route('/', methods=['GET', 'POST'])
 def home():
-    form=MyForm()
-    conta = ContasModel.query.all()    
-    return render_template('home.html', form=form,  contas=conta)
+    form = MyForm()
+    conta = ContasModel.query.all()
+    return render_template('home.html', form=form,  contas=conta, soma=soma())
+
 
 @gerenciar.route('/post', methods=['GET', 'POST'])
 def post():
     form = MyForm()
-    #estou pegando as informações dos formularios
+    # estou pegando as informações dos formularios
     if form.validate_on_submit:
         descricao = form.descricao.data
         preco = form.preco.data
         data_venc = form.data_venc.data
-        comment = form.comment.data     
-        #aqui é onde as informações serão enviadas para o banco
-        contas=ContasModel(descricao,preco, data_venc, comment)
-        db.session.add(contas)        
+        comment = form.comment.data
+        # aqui é onde as informações serão enviadas para o banco
+        contas = ContasModel(descricao, preco, data_venc, comment)
+        db.session.add(contas)
         db.session.commit()
-        #aqui é selecionado todas as informações do banco e salvo em alldata
+        # aqui é selecionado todas as informações do banco e salvo em alldata
         alldata = ContasModel.query.all()
-        listalldata=[]
+        listalldata = []
         for i in alldata:
             listalldata.append(i.descricao)
             listalldata.append(i.preco)
             listalldata.append(i.data_venc)
             listalldata.append(i.comment)
         print(listalldata)
-        #send_mail()
-    
+        # send_mail()
+
     return redirect('/')
+
+
 @gerenciar.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    print("UPDATE")
     form_up = MyForm()
-    if form_up.validate_on_submit:        
-        item=ContasModel.query.get_or_404(id)
+    if form_up.validate_on_submit:
+        item = ContasModel.query.get_or_404(id)
         item.descricao = form_up.descricao.data
-        item.preco = form_up.preco.data     
+        item.preco = form_up.preco.data
         item.data_venc = form_up.data_venc.data
-        item.comment = form_up.comment.data     
-        #aqui é onde as informações serão enviadas para o banco
-        print('Descriçao: ', item.descricao)
-        print("Preço: ", item.preco) 
-        print("Data_venc: ", item.data_venc) 
-        print("Comentário: ", item.comment)    
+        item.comment = form_up.comment.data
+        # aqui é onde as informações serão enviadas para o banco
         db.session.commit()
     return redirect('/')
+
 
 @gerenciar.route('/select/', methods=['GET', 'POST'])
 def select():
     form = MyForm()
-    #if form.validate_on_submit:
-    descricao=form.descricao_pesquise.data
-    preco=form.preco_pesquise.data
-    date=form.date_pesquise.data    
-    comment=form.comment_pesquise.data
+    # if form.validate_on_submit:
+    descricao = form.descricao_pesquise.data
+    preco = form.preco_pesquise.data
+    date = form.date_pesquise.data
+    comment = form.comment_pesquise.data
     if descricao:
-        print("descricao")
         descricao_pesquise = "%{}%".format(descricao)
-        item2=ContasModel.query.filter(ContasModel.descricao.like(descricao_pesquise)).all()
-        return render_template('return_filtro.html', item=item2)
-
+        item2 = ContasModel.query.filter(ContasModel.descricao.like(descricao_pesquise)).all()
+        return render_template('return_filtro.html', item=item2, soma = soma_filtro(ContasModel.descricao, descricao_pesquise))
 
     elif preco:
-        print("preco")
-        item1=ContasModel.query.filter_by(preco = preco).all()
-        return render_template('return_filtro.html', item=item1)
-   
-    
+        item1 = ContasModel.query.filter_by(preco=preco).all()
+        return render_template('return_filtro.html', item=item1, soma=soma_filtro(ContasModel.preco, preco))
+
     elif date:
-        print("date")
-        item=ContasModel.query.filter_by(data_venc=date).all()
-        return render_template('return_filtro.html', item=item)
+        data_pesquise = "%{}%".format(date)
+        item = ContasModel.query.filter(
+            ContasModel.data_venc.like(data_pesquise)).all()
+        return render_template('return_filtro.html', item=item, soma=soma_filtro(ContasModel.data_venc, data_pesquise))
 
-    
     elif comment:
-        print("comment")
         comment_pesquise = "%{}%".format(comment)
-        item3=ContasModel.query.filter(ContasModel.comment.like(comment_pesquise)).all()
-        return render_template('return_filtro.html', item=item3)
+        item3 = ContasModel.query.filter(ContasModel.comment.like(comment_pesquise)).all()
+        return render_template('return_filtro.html', item=item3, soma=soma_filtro(ContasModel.comment, comment_pesquise))
     else:
-       return "No DATA"
-
+       return render_template('no_data.html')
 
 
 @gerenciar.route('/delete/<int:id>', methods=['GET', 'POST'])
@@ -104,13 +99,35 @@ def delete_contas(id):
     db.session.delete(delete)
     db.session.commit()
 
-    return redirect ('/')
-
-
+    return redirect('/')
 
 
 def send_mail():
-   msg = Message('Hello', sender = "conta.casa89@gmail.com", recipients = ["fmfabio.0991@yahoo.com.br"])
+   msg = Message('Hello', sender="conta.casa89@gmail.com",
+                 recipients=["fmfabio.0991@yahoo.com.br"])
    msg.body = "Hello Flask message sent from Flask-Mail"
    mail.send(msg)
    return "Sent"
+
+# funções que retornam somatoria do campo preco
+def soma():
+    soma = db.session.query(func.sum(ContasModel.preco))
+    i=0
+    for i in soma:
+        pass
+    return i
+
+def soma_filtro(campo, data_form):
+    print('campo: ', campo)
+    if campo == 'preco':
+        resultado = ContasModel.query.filter_by(preco=preco).all()
+        valor=0
+        for i in resultado:
+            valor += i.preco
+        return valor
+    else:
+        resultado = ContasModel.query.filter(campo.like(data_form)).all()
+        valor=0
+        for i in resultado:
+            valor += i.preco
+        return valor
--- forms/form.py
+++ forms/form.py
@@ -3,12 +3,16 @@ from wtforms import StringField, DecimalField, DateTimeField, SubmitField, TextA
 from wtforms.validators import DataRequired
 
 class MyForm(FlaskForm):
-    descricao = StringField('Descrição', validators=[DataRequired()])
-    preco = StringField('Preço', validators=[DataRequired(message="Somente numeros e .")])
-    data_venc = DateField('Data Venc', validators=[DataRequired(message="Use - para separar a data! (09-02-1991")], format='%Y-%m-%d')
-    comment = StringField('Comentário')
-    filtro = StringField('Filtro')
+    descricao = StringField('Descrição:', validators=[DataRequired()])
+    preco = StringField('Valor:', validators=[DataRequired(message="Somente numeros e .")])
+    data_venc = DateField('Data Venc:', validators=[DataRequired(message="Use - para separar a data! (09-02-1991")], format='%Y-%m-%d')
+    comment = StringField('Comentário:')
+    date_pesquise = StringField('Data venc:')
+    descricao_pesquise = StringField('Descrição:')
+    preco_pesquise = StringField('Valor:')
+    comment_pesquise = StringField('Comentário:')
     submit = SubmitField('Gravar')
+    submit_pesquise = SubmitField('Pesquisar')
 
 
      
