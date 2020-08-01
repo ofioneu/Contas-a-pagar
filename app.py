353113036a9cafba1beb3bed37869cb590aa967d
Ofioneu <fmfabio.0991@yahoo.com.br>
2020-08-01 19:13:15 -0300

    adicionado caminho para arquivos staticos e atualizado requeriments.txt

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_mail import Mail
 config = app_config[app_active]
 
 def create_app(config_name):
-    app = Flask(__name__, template_folder='templates')
+    app = Flask(__name__, template_folder='templates', static_url_path='')
     app.secret_key = config.SECRET
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
